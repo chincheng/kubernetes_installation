fe334415c94a546a003f8758002b6aac8086dfc8
Chincheng <chen.chincheng@gmail.com>
2018-08-03 21:18:54 +0800

    add Env Variable - KUBERNETES_MASTER_IP

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -2,8 +2,9 @@
 
 sudo kubeadm reset
 
+KUBERNETES_MASTER_IP=192.168.1.26
 #sudo kubeadm init --apiserver-advertise-address=192.168.1.26
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.1.26
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$KUBERNETES_MASTER_IP
 
 rm $HOME/.kube/config
 
@@ -24,4 +25,3 @@ export KUBECONFIG=$HOME/.kube/config
 #wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 kubectl create -f  kube-flannel.yml
-
